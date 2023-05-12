430679a2ec377cfe5593aa3aced680fdd5287eef
Tony Fischetti <tony.fischetti@gmail.com>
2023-05-12 16:26:14 -0400

    now the display is just as pretty as before

--- src/step-1-clean-raw-logs-2023.cpp
+++ src/step-1-clean-raw-logs-2023.cpp
@@ -6,18 +6,53 @@
 #include <regex>
 #include <cmath>
 #include <algorithm>
+#include <csignal>
 
 #include <re2/re2.h>
 
 #include "glob.h"
+#include "indicators.hpp"
+#include "rang.hpp"
 
 
 using namespace std;
+using namespace rang;
+using namespace indicators;
+
+
+static ofstream outfile;
+
+static ProgressBar bar{
+        option::BarWidth{70},
+        option::Start{"["},
+        option::Fill{"■"},
+        option::Lead{"■"},
+        option::Remainder{"-"},
+        option::End{"]"},
+        option::PostfixText{""},
+        option::ForegroundColor{Color::yellow},
+        option::FontStyles{std::vector<FontStyle>{FontStyle::bold}}
+};
+
+
+
+string display_time() {
+    return ("[" + to_string(time(0)) + "]  ");
+}
+
+
+void handle_sigint(int signum) {
+    cerr << endl << endl << fg::red << display_time() << "caught SIGINT" << endl;
+    outfile.close();
+    indicators::show_console_cursor(true);
+    cerr << style::reset << endl;
+    exit(1);
+}
 
 
 const vector<string> get_files() {
     vector<string> input_files;
-    for (auto& p : glob::glob("./logs/i.ezproxy*.log")) {
+    for (auto& p : glob::glob("./logs/i.ezproxy.nypl.org.2023*.log")) {
         input_files.push_back(p);
     }
     sort(input_files.begin(), input_files.end());
@@ -52,10 +87,10 @@ void process_line(string* all_fields, const string& line) {
 
 
 const string fix_whole_date(const string& adate) {
-    const string day = adate.substr(1, 2);
+    const string day  = adate.substr(1, 2);
     const string year = adate.substr(8, 4);
     const string time = adate.substr(13, string::npos);
-    string premonth = adate.substr(4, 3);
+    string premonth   = adate.substr(4, 3);
 
     if      (premonth == "Jan") premonth = "01";
     else if (premonth == "Feb") premonth = "02";
@@ -77,24 +112,35 @@ const string fix_whole_date(const string& adate) {
 int main() {
     using namespace re2;
 
-    cout << endl << endl << "::alice glass:: HI!" << endl;
+    signal(SIGINT, handle_sigint);
+
+    cout << endl << endl << fg::gray << style::dim
+         << display_time() << "::alice glass:: HI!"
+         << endl << style::bold << fg::cyan << display_time()
+         << "Processing raw logs" << style::reset << endl << endl;
 
     const vector<string> input_files = get_files();
     const uint16_t count = input_files.size();
     const string last_date = input_files[count-1].substr(24, 10);
-    const string output_file = "intermediate/cpp-cleaned-logs-" + last_date + ".dat";
+    const string output_file = "intermediate/cleaned-logs-" + last_date + ".dat";
     const RE2 re1  { "^https?[^A-Za-z]*(.+?):\\d.+$" };
     double counter { 1.0f };
 
-    ofstream outfile(output_file);
+    outfile.open(output_file);
     outfile << make_tab_delimited("ip", "barcode", "session", "date_and_time",
                                   "url", "fullurl") << endl;
 
     // reusing this to store the necessary fields
     string tmp[7];
 
+    show_console_cursor(false);
+
     for (auto& item : input_files) {
-        cout << round(counter/count*100) << endl;
+        uint8_t perc = round(counter/count*100);
+        bar.set_progress(perc);
+        bar.set_option(option::PostfixText{ "  " + to_string(int(counter)) +
+                                            "/" + to_string(count) + "  " +
+                                            to_string(perc) + "%" });
 
         ifstream infile(item);
         string line;
@@ -121,7 +167,15 @@ int main() {
         }
         counter++;
     }
+    cout << endl << endl << style::bold << fg::cyan << display_time()
+         << "Closing output file" << endl;
+
     outfile.close();
+    show_console_cursor(true);
+
+    cout << style::bold << fg::green << display_time()
+         << "Done!" << endl;
+    cout << style::reset << fg::reset;
 }
 
 
